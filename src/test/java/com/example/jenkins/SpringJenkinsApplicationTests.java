76b8d5d246fc69931b73e5bf66d921e6b56dd617
madhumita bhatia <madhumeetabhatia@gmail.com>
2024-05-02 20:30:36 +0530

    second commit

--- src/test/java/com/example/jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/jenkins/SpringJenkinsApplicationTests.java
@@ -3,10 +3,12 @@ package com.example.jenkins;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.junit4.SpringRunner;
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 	
@@ -15,6 +17,7 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test Case executing..");
+		logger.info("Test Case executing second log statement..");
 		assertEquals(true,true);
 	}
 
